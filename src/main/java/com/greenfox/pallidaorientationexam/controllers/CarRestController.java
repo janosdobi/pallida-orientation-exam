96041c52a4937cc83cc0674d8ff978237d5e1ee6
Janos Dobi <j.dobi88@gmail.com>
2017-11-17 12:09:55 +0100

    add exception handling to restcontroller

--- src/main/java/com/greenfox/pallidaorientationexam/controllers/CarRestController.java
+++ src/main/java/com/greenfox/pallidaorientationexam/controllers/CarRestController.java
@@ -3,15 +3,23 @@ package com.greenfox.pallidaorientationexam.controllers;
 import com.greenfox.pallidaorientationexam.models.CarList;
 import com.greenfox.pallidaorientationexam.services.CarService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
 
 @RestController
 public class CarRestController {
 
     @Autowired
     CarService carService;
+
+    @ExceptionHandler(Exception.class)
+    public CarList displayStatusError() {
+        return new CarList("Error. Something went wrong. Sorry :(", Arrays.asList());
+    }
 
     @GetMapping("/api/search/{brand}")
     public CarList listCarsByBrand(@PathVariable String brand) {
