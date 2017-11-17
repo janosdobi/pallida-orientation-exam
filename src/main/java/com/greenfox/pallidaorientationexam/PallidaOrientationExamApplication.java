972bb4e65da38d4b5001299e2193ab950b72f7e8
Janos Dobi <j.dobi88@gmail.com>
2017-11-17 09:27:24 +0100

    init commit w database screenshot

---
+++ src/main/java/com/greenfox/pallidaorientationexam/PallidaOrientationExamApplication.java
@@ -0,0 +1,12 @@
+package com.greenfox.pallidaorientationexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PallidaOrientationExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PallidaOrientationExamApplication.class, args);
+	}
+}
---
+++ src/test/java/com/greenfox/pallidaorientationexam/PallidaOrientationExamApplicationTests.java
@@ -0,0 +1,16 @@
+package com.greenfox.pallidaorientationexam;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PallidaOrientationExamApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
